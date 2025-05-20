07cd82e7baf444cff048da87bdf7d489c1bc3d95
hisoka <hisoka>
2025-05-20 17:37:53 +0200

    finish hero section plus projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Hero from "./sections/Hero";
 import React from "react";
+import ShowcaseSection from "./sections/ShowcaseSection";
 
 const App = () => {
   return (
     <>
       <Hero />
+      <ShowcaseSection />
     </>
   );
 };
